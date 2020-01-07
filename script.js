93c1d94acb2d71889bcdcf951ea39e841515c7d9
flaviocomi <flabio.co91@gmail.com>
2020-01-07 14:01:35 +0100

    init

---
+++ script.js
@@ -0,0 +1,5 @@
+// GOAL:
+// creare una pagina che stampi tutte le configurazioni presenti nel DB. Dare inoltre la possibilita' di aggiungere una nuova configurazione (titolo e descrizione) e tentare l'approccio per una update (aggiornamento di una configurazione esistente).
+
+// BONUS:
+// in caso di prematura riuscita di tutto l'esercizio procedere con l'eliminazione di una configurazione esistente
